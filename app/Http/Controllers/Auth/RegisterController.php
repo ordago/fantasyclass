c7833f38c581fb590e560a3e5904c38540fa4bc9
Joan <joacliba@gmail.com>
2020-09-17 10:51:05 +0200

    Username and email lowercase

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -79,7 +79,7 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
-            'email' => $data['email'],
+            'email' => strtolower($data['email']),
             'username' => strtolower($data['username']),
             'password' => Hash::make($data['password']),
             'locale' => $data['locale'],
