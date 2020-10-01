3d726cc18b57a26b39eef9839561fa7fece51f15
Joan <joan.climent@copernic.cat>
2020-10-01 09:26:03 +0200

    Remoche chatId

--- resources/views/studentsview/index.blade.php
+++ resources/views/studentsview/index.blade.php
@@ -11,10 +11,9 @@
               /* Chatbro Widget Embed Code Start */
               function ChatbroLoader(chats,async){async=!1!==async;var params={embedChatsParameters:chats instanceof Array?chats:[chats],lang:navigator.language||navigator.userLanguage,needLoadCode:'undefined'==typeof Chatbro,embedParamsVersion:localStorage.embedParamsVersion,chatbroScriptVersion:localStorage.chatbroScriptVersion},xhr=new XMLHttpRequest;xhr.withCredentials=!0,xhr.onload=function(){eval(xhr.responseText)},xhr.onerror=function(){console.error('Chatbro loading error')},xhr.open('GET','//www.chatbro.com/embed.js?'+btoa(unescape(encodeURIComponent(JSON.stringify(params)))),async),xhr.send()}
               // /* Chatbro Widget Embed Code End */
-              ChatbroLoader({encodedChatId: '94Un6'});
               ChatbroLoader({
                      chatTitle: "{{ $chat }}",
-                     parentEncodedChatId: '94Un6',
+                     parentEncodedChatId: "{{ env('CHATBRO_ID') }}",
                      siteDomain: "{{ $url }}",
                      siteUserExternalId: "{{ auth()->user()->id }}-{{ $student->id }}",
                      siteUserFullName: "{{ $student->name }}",
