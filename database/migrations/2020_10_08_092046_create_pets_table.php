054321748da3df8c74a1e8a22e5a611c8c619af8
Joan <joan.climent@copernic.cat>
2020-10-08 11:32:55 +0200

    Added sprites and start pets

---
+++ database/migrations/2020_10_08_092046_create_pets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->id();
+            $table->string('image');
+            $table->float('hp')->default(100);
+            $table->float('hp_boost');
+            $table->float('xp_boost');
+            $table->float('gold_boost');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pets');
+    }
+}
