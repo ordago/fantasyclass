630883aa29d71d89f05d63073af8a381b2e47339
Joan <joan.climent@copernic.cat>
2020-10-19 17:11:50 +0200

    Trying to connect to goole

--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -2,17 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use Google_Service;
 use Google_Service_Classroom;
+use Google_Service_Drive;
+use Google_Service_Oauth2;
 use Illuminate\Http\Request;
 use PulkitJalan\Google\Facades\Google;
 
 class ServicesController extends Controller
 {
-    public function drive() {
-        // $classroom = Google::make('classroom');
+    public function gclassroom() {
+        $classroom = Google::make('classroom');
 
-        // $client = new \PulkitJalan\Google\Client($config);
-        // $googleClient = $client->getClient();
+        $googleClient = Google::getClient();
+        $googleClient->setAuthConfig('/var/www/google.json');
+        // $googleClient->setScopes([
+        //     'https://www.googleapis.com/auth/classroom.courses.readonly',
+        //     "https://www.googleapis.com/auth/classroom.rosters.readonly",
+        //     'https://www.googleapis.com/auth/classroom.coursework.students',
+        //     'https://www.googleapis.com/auth/classroom.student-submissions.students.readonly',
+        //     'https://www.googleapis.com/auth/classroom.announcements',
+        //   ]);
+        $googleClient->addScope(Google_Service_Drive::DRIVE);
+
+        $redirect_uri = "http://localhost:8000";
+        $googleClient->setRedirectUri($redirect_uri);
+        $googleClient->getAccessToken();
+
+        // $httpClient = $googleClient->authorize();
+
+        // $google_oauthV2 = new Google_Service_Oauth2($googleClient);
+        // dump($google_oauthV2);
+
+        // if (isset($_GET['code'])) {
+        //     $token = $googleClient->fetchAccessTokenWithAuthCode($_GET['code']);
+        // }
 
     }
 }
