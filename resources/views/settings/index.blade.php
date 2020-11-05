a8f78f440b34c83321a4a2760e098fe07bf9d20c
Ignacio Salazar Calle <nagdalion@gmail.com>
2020-11-05 21:27:25 +0100

    Breadcumb Settings

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts.teacher')
 
+@section('breadcrumb')
+@include('layouts.breadcrumbstart')
+<li>
+    <a href="/classroom/{{ $class->code }}">
+        <span class="icon is-small">
+            <i class="fad fa-home" aria-hidden="true"></i>
+        </span>
+        <span>{{ __('general.home') }}</span>
+    </a>
+</li>
+<li class="is-active">
+    <a href="#">
+        <span class="icon is-small">
+            <i class="fad fa-file-chart-line" aria-hidden="true"></i>
+        </span>
+        <span>{{ __('menu.settings') }}</span>
+    </a>
+</li>
+@include('layouts.breadcrumbend')
+@endsection
+
 @section('content')
 <settings :settings="{{ json_encode($settings) }}" :teachers="{{ $teachers }}" :user="{{ auth()->user() }}" :is-admin="{{ $isAdmin ?? 0 }}" :classroom="{{ $class }}"></settings>
 @endsection
