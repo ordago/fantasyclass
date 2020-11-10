585d2748d52c98779a86209ce702258108cef8c2
Joan <joan.climent@copernic.cat>
2020-11-10 10:25:14 +0100

    added manifest to local

--- resources/views/auth/home.blade.php
+++ resources/views/auth/home.blade.php
@@ -12,6 +12,25 @@
     <!-- <link rel="stylesheet" type="text/css" href="/css/app.css" /> -->
     <script src="https://kit.fontawesome.com/90342cb187.js" crossorigin="anonymous"></script>
     <link rel="icon" type="image/png" href="favicon.png" />
+
+    <!-- Mobile -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2, user-scalable=1">
+
+
+
+    <!-- PWA -->
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="apple-mobile-web-app-title" content="FantasyClass">
+    <meta name="application-name" content="FantasyClass">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+
 </head>
 
 <body>
