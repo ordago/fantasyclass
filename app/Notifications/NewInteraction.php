32cf9718e52f0f1182dae2dd407507f6bfe2c6af
Joan <joan.climent@copernic.cat>
2020-11-12 23:22:33 +0100

    Try mono icon

--- app/Notifications/NewInteraction.php
+++ app/Notifications/NewInteraction.php
@@ -71,8 +71,8 @@ class NewInteraction extends Notification
 
         return (new WebPushMessage)
         ->title($this->title)
-        ->icon('/notification.png')
-        ->badge('/notification.png')
+        ->icon('/ic_fc_mono.png')
+        ->badge('/ic_fc_mono.png')
         ->body($this->from['name'] . ": ".$this->content)
         ->data($data)
         ->action('Open FantasyClass', $action);
