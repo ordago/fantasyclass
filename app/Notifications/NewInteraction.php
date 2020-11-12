e36f9b9739e1c2265e75b59adac42a777174d50e
Joan <joan.climent@copernic.cat>
2020-11-12 23:03:42 +0100

    Remove badge

--- app/Notifications/NewInteraction.php
+++ app/Notifications/NewInteraction.php
@@ -72,7 +72,6 @@ class NewInteraction extends Notification
         return (new WebPushMessage)
         ->title($this->title)
         ->icon('/notification.png')
-        ->badge('/notification.png')
         ->body($this->from['name'] . ": ".$this->content)
         ->data($data)
         ->action('Open FantasyClass', $action);
