7788f017bf6ac3cf01a8923a314945b94ca59943
Joan <joan.climent@copernic.cat>
2021-05-18 09:52:37 +0200

    Badge

--- app/Notifications/NewMessage.php
+++ app/Notifications/NewMessage.php
@@ -68,7 +68,7 @@ class NewMessage extends Notification
         return (new WebPushMessage)
         ->title('New message')
         ->body($this->from['name'] . ": ".$this->content)
-        ->badge('/img/bg-xp.png')
+        ->badge(1)
         ->icon('/ic_fc_mono.png')
         ->data($data)
         ->action('FantasyClass', $action);
