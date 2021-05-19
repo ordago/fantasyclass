6607b4756d26c3f52a1f4dd643579b49518bdc06
Joan <joan.climent@copernic.cat>
2021-05-19 09:37:35 +0200

    script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -211,9 +211,7 @@
         });
 
         function isItIE() {
-            user_agent = navigator.userAgent;
-            var is_it_ie = user_agent.indexOf("MSIE ") > -1 || user_agent.indexOf("Trident/") > -1;
-            return is_it_ie;
+            return /Trident\/|MSIE/.test(window.navigator.userAgent);
         }
         if (isItIE()) {
             document.getElementById('ieAlert').style = "";
