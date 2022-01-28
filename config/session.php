d5aa90857c072849cb1909bfe333270fee0b0c32
Joan <joan.climent@copernic.cat>
2022-01-28 16:29:04 +0100

    rollback to lax

--- config/session.php
+++ config/session.php
@@ -166,7 +166,7 @@ return [
     |
     */
 
-    'secure' => true,
+    'secure' => env('SESSION_SECURE_COOKIE'),
 
     /*
     |--------------------------------------------------------------------------
@@ -194,6 +194,6 @@ return [
     |
     */
 
-    'same_site' => "none",
+    'same_site' => "lax",
 
 ];
